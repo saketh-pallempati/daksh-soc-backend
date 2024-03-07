98fab2f269b657631b515240395ed3918614219e
Saketh Pallempati <90235687+SAKETH-PALLEMPATI@users.noreply.github.com>
2024-03-07 20:57:25 +0530

    cors1

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(
   cors({
     origin: ["http://localhost:5173"],
     credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
 app.use(cookieParser());
