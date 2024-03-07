1f732b86ab8611a075f10cb80f2c858bb08e6a80
Saketh Pallempati <90235687+SAKETH-PALLEMPATI@users.noreply.github.com>
2024-03-07 20:49:32 +0530

    cors

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ],
+    origin: ["*"],
     credentials: true,
   })
 );
